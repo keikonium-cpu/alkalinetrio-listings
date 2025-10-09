3f98ed09ae5005e4a45fc618d8ed678a63667ff8
keikonium-cpu <keikonium@gmail.com>
2025-10-09 19:43:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ cloudinary.config({
 });
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    defaultViewport: { width: 1280, height: 1080 },
-  });
+const browser = await puppeteer.launch({
+  headless: true,
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  defaultViewport: { width: 1280, height: 1080 },
+});
 
   const page = await browser.newPage();
   console.log(`Loading ${URL} ...`);
